7f0138802ac7ed09d2bbc84f44fba9e396dc49c4
V <vishnubhatt@gmail.com>
2017-09-30 18:02:08 -0700

    First cut

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -1,3 +1,4 @@
+import urllib2
 import urllib
 import logging
 
@@ -9,11 +10,10 @@ class Exchange:
     pass
 
 def show_markets():
-    print 'showing markets'
     exchange_names = ["BitfinexUSD",
                       "BitstampUSD",
                       "KrakenUSD",
-                      "OKCoinCNY"]
+                      "OKCoinUSD"]
     exchanges = get_exchanges(exchange_names)
     return render_template('exchanges.html',
                             exchanges=exchanges)
@@ -22,23 +22,60 @@ def get_exchanges(exchange_names):
     exchanges = []
     for iexchange in exchange_names:
         exchange = get_exchange(iexchange)
-        exchanges.append(exchange)
+        if exchange:
+            exchanges.append(exchange)
     return exchanges
 
 def get_exchange(exchange_name):
-    res = urllib.urlopen('https://api.bitfinex.com/v1/book/btcusd')
-    jsonstr = res.read().decode('utf8')
+    jsonstr = ""
     try:
-        raw_exchange = json.loads(jsonstr)
-        exchange = raw_exchange.get('asks')
-        if exchange:
-            rexchange = Exchange()
-            rexchange.name = exchange_name
-            rexchange.cc = "BTC"
-            rexchange.time = exchange[0].get('timestamp')
-            rexchange.price = exchange[0].get('price')
-            rexchange.currency = "USD"
-            return rexchange
+        if exchange_name == 'BitstampUSD' or exchange_name == 'KrakenUSD' or exchange_name == 'OKCoinCNY':
+            if exchange_name == 'KrakenUSD':
+                url = 'https://api.kraken.com/0/public/Depth?pair=XXBTZUSD'
+            elif exchange_name == 'BitstampUSD':
+                url = 'https://www.bitstamp.net/api/order_book/'
+            elif exchange_name == 'OKCoinUSD':
+                url = 'https://www.okcoin.cn/api/depth.do?symbol=btc_usd'
+            req = urllib2.Request(url, None, headers={
+                                        "Content-Type": "application/x-www-form-urlencoded",
+                                        "Accept": "*/*",
+                                        "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
+
+            res = urllib2.urlopen(req)
+            jsonstr = res.read().decode('utf8')
+            print jsonstr
+            raw_exchange = json.loads(jsonstr)
+            if raw_exchange:
+                rexchange = Exchange()
+                rexchange.name = exchange_name
+                rexchange.cc = "BTC"
+                rexchange.currency = "USD"
+                if exchange_name == 'OKCoinUSD':
+                    rexchange.price = raw_exchange.get('asks')[0][0]
+                elif exchange_name == 'KrakenUSD':
+                    rexchange.time  = raw_exchange.get('result').get('XXBTZUSD').get('asks')[0][2]
+                    rexchange.price = raw_exchange.get('result').get('XXBTZUSD').get('asks')[0][0]
+                else:
+                    rexchange.time = raw_exchange.get('timestamp')
+                    rexchange.price = raw_exchange.get('asks')[0][0]
+                return rexchange
+            else:
+                return None
+        else:
+            res = urllib.urlopen('https://api.bitfinex.com/v1/book/btcusd')
+            jsonstr = res.read().decode('utf8')
+            raw_exchange = json.loads(jsonstr)
+            exchange = raw_exchange.get('asks')
+            if exchange:
+                rexchange = Exchange()
+                rexchange.name = exchange_name
+                rexchange.cc = "BTC"
+                rexchange.time = exchange[0].get('timestamp')
+                rexchange.price = exchange[0].get('price')
+                rexchange.currency = "USD"
+                return rexchange
+            else:
+                return None
     except Exception:
         logging.error("%s - Can't parse json: %s" % (exchange_name, jsonstr))
         return None
